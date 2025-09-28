d7525cb30e6720781d3fa5d37ab7f349da93431f
Murtaza Dhuliawala <murtazad@Murtazas-Personal-Macbook-Pro-2.local>
2025-09-28 14:13:19 -0700

    Adding pretokenization for BPE tokenizer training

---
+++ stanford_courses/cs336_language_modeling_from_scratch/assignment1-basics/cs336_basics/pretokenization.py
@@ -0,0 +1,113 @@
+import os
+from typing import BinaryIO
+import regex as re
+from collections import Counter
+
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+def pretokenize_one_chunk(text_chunk, special_tokens, tokenizer_re):
+    """Tokenizes a chunk skipping special tokens and returns the pre-tokenized text as a list of strings."""
+    tokenized_text = []
+    for m in tokenizer_re.finditer(text_chunk):
+        cur_token = m.group(0)
+        if special_tokens and cur_token in special_tokens:
+            continue
+        tokenized_text.append(cur_token)
+    
+    return tokenized_text
+
+
+def get_pretokenized_corpus(input_corpus_filepath, special_tokens = None):
+    with open(input_corpus_filepath, "rb") as f:
+        num_processes = 5
+        # Consider the `<|endoftext|>` as a special case and let's use the code provided.
+        # Edge case would be to handle some different token when doing the chunking.
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+
+        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+        if not special_tokens:
+            combined_pat = PAT
+        else:
+            escaped_specials = [re.escape(tok) for tok in special_tokens]
+            combined_pat = r"(?:{})|(?:{})".format("|".join(escaped_specials), PAT)
+        
+        tokenizer_re = re.compile(combined_pat)
+
+        pretokenized_counter = Counter()
+        string_counter = Counter()
+
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            # print(f"\n\nChunk is:\n{chunk}")
+            
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
+            tokenized_chunk = pretokenize_one_chunk(chunk, special_tokens, tokenizer_re)
+            # print(f"\nTokenized:\n{tokenized_chunk}")
+
+            for token in tokenized_chunk:
+                token_byte_tuple = tuple(token.encode("utf-8"))
+                pretokenized_counter[token_byte_tuple] += 1
+                string_counter[token] += 1
+
+        # print("\nFinal bytes counter:")
+        # print(pretokenized_counter)
+
+        print("\nFinal string counter:")
+        print(string_counter)
+        
+        return pretokenized_counter
+
+
+# Example usage.
+# corpus_filepath = "/Users/murtazad/Desktop/Murtaza/studies/ml_data/cs336_llms_from_scratch/assignment1_basics_data/TinyStoriesV2-GPT4_small.txt"
+corpus_filepath = "/Users/murtazad/Desktop/Murtaza/studies/ml_data/cs336_llms_from_scratch/assignment1_basics_data/TinyStoriesV2-GPT4-valid.txt"
+pretokenized_counter = get_pretokenized_corpus(corpus_filepath, ["<|endoftext|>"])
