2d4bf2c880cae434942ef26298e4ce7a353ae3c5
Murtaza Dhuliawala <murtazad@Murtazas-Personal-Macbook-Pro-2.local>
2025-10-07 23:39:37 -0700

    Comment out main in pretokenize to enable imports

--- stanford_courses/cs336_language_modeling_from_scratch/assignment1-basics/cs336_basics/pretokenization_parallel.py
+++ stanford_courses/cs336_language_modeling_from_scratch/assignment1-basics/cs336_basics/pretokenization_parallel.py
@@ -113,13 +113,13 @@ def get_pretokenized_corpus_parallel(input_corpus_filepath, special_tokens, num_
 
     return pretokenized_counter
 
-if __name__ == "__main__":
-    # Example usage.
-    # corpus_filepath = "/Users/murtazad/Desktop/Murtaza/studies/ml_data/cs336_llms_from_scratch/assignment1_basics_data/TinyStoriesV2-GPT4_small.txt"
-    corpus_filepath = "/Users/murtazad/Desktop/Murtaza/studies/ml_data/cs336_llms_from_scratch/assignment1_basics_data/TinyStoriesV2-GPT4-valid.txt"
-    pretokenized_counter = get_pretokenized_corpus_parallel(input_corpus_filepath=corpus_filepath, special_tokens=["<|endoftext|>"], num_processes=5)
+# if __name__ == "__main__":
+#     # Example usage.
+#     # corpus_filepath = "/Users/murtazad/Desktop/Murtaza/studies/ml_data/cs336_llms_from_scratch/assignment1_basics_data/TinyStoriesV2-GPT4_small.txt"
+#     corpus_filepath = "/Users/murtazad/Desktop/Murtaza/studies/ml_data/cs336_llms_from_scratch/assignment1_basics_data/TinyStoriesV2-GPT4-valid.txt"
+#     pretokenized_counter = get_pretokenized_corpus_parallel(input_corpus_filepath=corpus_filepath, special_tokens=["<|endoftext|>"], num_processes=5)
 
-    for token_tuple, count in pretokenized_counter.most_common(300):
-        cur_token = bytes(token_tuple).decode("utf-8")
-        print(f"{cur_token}:\t{count}")
+#     for token_tuple, count in pretokenized_counter.most_common(300):
+#         cur_token = bytes(token_tuple).decode("utf-8")
+#         print(f"{cur_token}:\t{count}")
 
